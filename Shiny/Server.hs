07d9168f98e2cd981c2265add539514302e97d1b
Diony Rosa <dhrosa@gmail.com>
2013-12-26 03:09:07 -0500

    Initial commit. Basic echo-ing TCP server.

---
+++ Shiny/Server.hs
@@ -0,0 +1,22 @@
+import Network (listenOn, withSocketsDo, accept, PortID(..), Socket)
+import System.Environment (getArgs)
+import System.IO (hSetBuffering, hGetLine, hPutStrLn, BufferMode(..), Handle)
+import Control.Concurrent (forkIO)
+
+echo :: Handle -> IO()
+echo client = do
+  line <- hGetLine client
+  hPutStrLn client line
+  echo client
+
+clientHandler :: Socket -> IO()
+clientHandler sock = do
+  (client, _, _) <- accept sock
+  hSetBuffering client NoBuffering
+  forkIO $ echo client
+  clientHandler sock
+
+main = do
+  socket <- listenOn $ PortNumber 3000
+  putStrLn $ "Server started."
+  clientHandler socket
