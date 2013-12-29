e86512cbba6afb5b53368416295cc90b4ac9f59a
Diony Rosa <dhrosa@gmail.com>
2013-12-28 23:40:05 -0500

    Changed ListFocus to a more general and flexible type in its own module.

---
+++ Shiny/Focus.hs
@@ -0,0 +1,78 @@
+-- | Functions for performing operations on a subsection of a list
+module Shiny.Focus (
+  Focus,
+  -- * Creating a focus
+  onIndices,
+  range,
+  slice,
+  -- * Transforming focuses
+  shiftRight,
+  shiftLeft,
+  shift,
+  alsoOn,
+  exceptOn,
+  -- * Focus utilities
+  between,
+  apply,
+  unfocus
+  ) where
+
+-- | Represents a subsection of a list
+data Focus a = Focus {
+  indices :: [Int],
+  list :: [a]
+  } deriving (Show)
+
+-- | Constructs a Focus using the indices that satisfy a condition
+onIndices :: (Int -> Bool) -> [a] -> Focus a
+onIndices pred l = Focus (filter pred [0..length l - 1]) l
+
+-- | Focuses on the list between the first bound, inclusive, and the second, exclusive
+-- | This is equivalent to onIndices
+range :: Int -> Int -> [a] -> Focus a
+range start end = Focus [start..end-1]
+
+-- | Emulation of python slices.
+slice :: (Int, Int, Int) -- ^ The inclusive start, exclusive start, and the step between indices
+         -> [a] -- ^ The list to focus on
+         -> Focus a
+slice (start, end, step) = Focus [start,start+step..end-1]
+
+-- | Shifts each index in a focus to the right by n
+shiftRight :: Int -> Focus a -> Focus a
+shiftRight n (Focus i l) = Focus (map (+n) i) l
+
+-- | Shifts each index in a focus to the left by n
+shiftLeft :: Int -> Focus a -> Focus a
+shiftLeft n = shiftRight (-n)
+
+-- | Shifts each index in a focus to the right if n is positive, otherwise to the left
+shift :: Int -> Focus a -> Focus a
+shift = shiftRight
+
+-- | Adds indices satisfying the predicates
+alsoOn :: (Int -> Bool) -> Focus a -> Focus a
+alsoOn pred (Focus i l) = Focus (i ++ filter pred [0..length l - 1]) l
+
+-- | Removes the indices satisfying the predicate
+exceptOn :: (Int -> Bool) -> Focus a -> Focus a
+exceptOn pred (Focus i l) = Focus (filter (not.pred) i) l
+
+-- | Creates a predicate on indices between two indices
+between :: Int    -- ^ The start, inclusive
+           -> Int -- ^ The end, inclusive
+           -> Int -- ^ The index
+between start end i = start <= i && i < end
+
+-- | Applies a function to the elements in focus
+apply :: (a -> a) -> Focus a -> Focus a
+apply func (Focus ilist list) = Focus ilist newList
+  where
+    newList = zipWith indexFunc [0..] list
+    indexFunc i x = if i `elem` ilist
+                    then func x
+                    else x
+                         
+-- | Converts a focus back into a list
+unfocus :: Focus a -> [a]
+unfocus (Focus _ l) = l
--- Shiny/Shiny.hs
+++ Shiny/Shiny.hs
@@ -1,5 +1,5 @@
 -- | Helper methods for operating on LED displays
-module Data.Shiny  where
+module Shiny where
 
 import Data.Colour.RGBSpace
 import Data.Word (Word8)
@@ -27,54 +27,3 @@ rotate :: Int -> [a] -> [a]
 rotate n l
   | n > 0     = rotateRight n l
   | otherwise = rotateLeft (-n) l
-                
--- | Data type for operating on slices of a list
-data ListFocus a b =
-  ListFocus { left :: [a],
-              middle :: [b],
-              right :: [a]}                 
-
-instance (Show a, Show b) => Show (ListFocus a b) where
-  show (ListFocus l m r) = "Left: " ++ show l
-                           ++ ", Middle: " ++ show m
-                           ++ ", Right: " ++ show r
-
-instance Functor (ListFocus a) where
-  fmap f (ListFocus l m r) = ListFocus l (map f m) r
-  
--- | Focuses in on a part of a list
-focus :: (Int, Int) -- ^ The start and end indices to focus on
-         -> [a] -- ^ The list
-         -> ListFocus a a 
-focus (start, end) l = ListFocus left mid right
-  where
-    (left, rest) = splitAt start l
-    (mid, right) = splitAt (end - start) rest
-    
--- | Takes a focused list and returns the list itself
-unfocus :: ListFocus a a -> [a]
-unfocus (ListFocus l m r) = l ++ m ++ r
-
--- | Unfocus, then re-focus on different bounds
-refocus :: (Int, Int) -> ListFocus a a -> ListFocus a a
-refocus bounds = focus bounds . unfocus
-
--- | Shifts a focus by the given amount
--- | If the amount is positive, a right shift is performed, otherwise a left shift
-shiftFocus :: Int -> ListFocus a a -> ListFocus a a
-shiftFocus n foc = refocus (start+n, end+n) foc
-  where
-    start = length (left foc)
-    end = start + length (middle foc)
-
--- | Shifts a focus to the left by the given amount
-shiftFocusLeft :: Int -> ListFocus a a -> ListFocus a a
-shiftFocusLeft n = shiftFocus (-n)
-
--- | Shifts a focus to the right by the given amount
-shiftFocusRight :: Int -> ListFocus a a -> ListFocus a a
-shiftFocusRight = shiftFocus
-
--- | Applies a function to a list only on the given bounds
-applyOn :: (a -> a) -> (Int, Int) -> [a] -> [a]
-applyOn f bounds = unfocus . fmap f . focus bounds
